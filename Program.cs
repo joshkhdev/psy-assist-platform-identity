598924bf76f50ad43ea950ac3a3f1a3f1dfd7d96
Alexey@Work <ace@live.ru>
2024-06-14 21:26:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using AuthService;
+using IdentityServer4.Validation;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+// Настройка CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+// Настройка IdentityServer4
+builder.Services.AddIdentityServer()
+     .AddSigningCredential(new X509Certificate2("certificate.pfx", "Psy!A55ist"))
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes);
+
+builder.Services.AddTransient<ICustomTokenRequestValidator, CustomTokenRequestValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
