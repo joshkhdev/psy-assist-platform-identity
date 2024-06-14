1273f8a7cc15c382ddab48691e5d0cd0bbee06a2
Alexey@Work <ace@live.ru>
2024-06-14 21:30:05 +0300

    code clean up

--- Config.cs
+++ Config.cs
@@ -5,21 +5,18 @@ namespace AuthService;
 
 public class Config
 {
-    // Identity resources are data like user ID, name, email, profile picture, etc.
     public static IEnumerable<IdentityResource> IdentityResources =>
         [
         new IdentityResources.OpenId(),
         new IdentityResources.Profile(),
     ];
 
-    // API scopes are the permissions that the client application is requesting to access the API.
     public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
     {
         new ApiScope("myApi.read"),
         new ApiScope("myApi.write")
     };
 
-    // API resources are the actual APIs that the client application is requesting to access.
     public static IEnumerable<ApiResource> ApiResources =>
         new ApiResource[]
         {
@@ -30,20 +27,19 @@ public class Config
             }
         };
 
-    // Clients are the applications that will be requesting the API.
-    public static IEnumerable<Client> Clients =>
+    public static IEnumerable<Client> Clients => 
         new Client[]
         {
-              new Client
-                    {
-                        ClientId = "cwm.client",
-                        ClientName = "Client Credentials Client",
-                        AllowedGrantTypes = GrantTypes.ClientCredentials,
-                        ClientSecrets = { new Secret("secret".Sha256()) },
-                        AllowedScopes = { "myApi.read" },
-                        AccessTokenLifetime = 60,
-                        IdentityTokenLifetime = 60,
-                        AlwaysIncludeUserClaimsInIdToken = true,
-                    }
+            new Client
+            {
+                ClientId = "cwm.client",
+                ClientName = "Client Credentials Client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "myApi.read" },
+                AccessTokenLifetime = 60,
+                IdentityTokenLifetime = 60,
+                AlwaysIncludeUserClaimsInIdToken = true,
+            }
         };
 }
